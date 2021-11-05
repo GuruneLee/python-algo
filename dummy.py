5bf73e1cddb19aa64c8d990b307f4a725eae57c9
gurunelee <dlckdgk4858@gmail.com>
2021-11-06 01:08:01 +0900

    문자열장식

--- dummy.py
+++ dummy.py
@@ -1,26 +1,6 @@
-n = 5
-l = list(map(int, input().split()))
+n = int(input())
 
-
-def gcd(a, b):
-    while b > 0:
-        r = a % b
-        a = b
-        b = r
-    return a
-
-
-def lcm(a, b):
-    return (a*b)//gcd(a, b)
-
-
-m = 987654321
-for i in range(3):
-    for j in range(i+1, 4):
-        for k in range(j+1, 5):
-            l1 = l[i]
-            l2 = l[j]
-            l3 = l[k]
-            ll = lcm(l1, lcm(l2, l3))
-            m = min(m, ll)
-print(m)
+l = ["", "Aa", "AAa"]
+print(l)
+l.sort()
+print(l)
