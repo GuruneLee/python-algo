0a530f76ee096acb243e5b7ee4d68bd1bb940115
gurunelee <dlckdgk4858@gmail.com>
2021-11-17 02:13:27 +0900

    update

--- dummy.py
+++ dummy.py
@@ -1,36 +1,29 @@
-# 피보나치수6
-# 수학 - 행렬
-# 1 1 x f(n-1) = f(n-1) + f(n-2) = f(n)
-# 1 0   f(n-2)   f(n-1)            f(n-1)
+# 16953 A에서B
+from queue import Queue
 
-import sys
-sys.setrecursionlimit(10000)
+a, b = input().split()
 
-mod = 1000000007
-n = int(input())
+dp = {}
+dp[a] = 0
+q = Queue()
+q.put(a)
 
-
-def calM(a, b):
-    return [
-        (a[0]*b[0] + a[1]*b[2]) % mod,
-        (a[0]*b[1] + a[1]*b[3]) % mod,
-        (a[2]*b[0] + a[3]*b[2]) % mod,
-        (a[2]*b[1] + a[3]*b[3]) % mod
-    ]
-
-
-def Mat(k):
-    if k == 0:
-        return [1, 1, 1, 1]
-    if k == 1:
-        return [1, 1, 1, 0]
-    if k % 2 == 0:
-        tmp = Mat(k//2)
-        return calM(tmp, tmp)
-    else:
-        tmp = Mat(k-1)
-        return calM(tmp, [1, 1, 1, 0])
-
-
-m = Mat(n-1)
-print(m[0] % mod)
+while not q.empty():
+    cs = q.get()
+    ns = cs + '1'
+    if ns == b:
+        print(dp[cs]+2)
+        exit(0)
+    if int(ns) < int(b):
+        if ns not in dp:
+            dp[ns] = dp[cs]+1
+            q.put(ns)
+    ns = str(int(cs)*2)
+    if ns == b:
+        print(dp[cs]+2)
+        exit(0)
+    if int(ns) < int(b):
+        if ns not in dp:
+            dp[ns] = dp[cs]+1
+            q.put(ns)
+print(-1)
