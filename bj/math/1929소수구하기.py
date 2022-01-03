cdea58279231bf142df8ad741691cdeca6877b31
gurunelee <dlckdgk4858@gmail.com>
2022-01-04 01:52:03 +0900

    ㅁㄷㅇ

---
+++ bj/math/1929소수구하기.py
@@ -0,0 +1,18 @@
+# 에라토스테네스의 채
+
+n, m = map(int, input().split())
+
+p = [True] * (m+1)
+p[1] = False
+for i in range(2, m//2+1):
+    mul = 2
+    while True:
+        cur = mul*i
+        if cur > m:
+            break
+        p[cur] = False
+        mul += 1
+
+for i in range(n, m+1):
+    if p[i]:
+        print(i)
--- dummy.py
+++ dummy.py
@@ -1,16 +1,14 @@
-n, m = map(int, input().split())
+n = int(input())
+if n == 1:
+    exit(0)
 
-p = [True] * (m+1)
-p[1] = False
-for i in range(2, m//2+1):
-    mul = 2
-    while True:
-        cur = mul*i
-        if cur > m:
+
+def f(x):
+    for i in range(2, n+1, 1):
+        if x % i == 0:
+            print(i)
+            f(x//i)
             break
-        p[cur] = False
-        mul += 1
 
-for i in range(n, m+1):
-    if p[i]:
-        print(i)
+
+f(n)
