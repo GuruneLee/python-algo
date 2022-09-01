e799dce395115270f9590e9a300d761b9e97242c
ChangHa Lee <60911416+GuruneLee@users.noreply.github.com>
2022-09-02 01:05:08 +0900

    Create Add_Two_Numbers.py

--- leet/Add_Two_Numbers.py
+++ leet/Add_Two_Numbers.py
@@ -1,25 +1,21 @@
 ## Add Two Numbers
 ## https://leetcode.com/problems/add-two-numbers/submissions/
-
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        c = ListNode()
-        h = c
-        while l1:
-            c.val = l1.val
-            l1 = l1.next
-            if not l1: break
-            c.next = ListNode()
-            c = c.next
-        c = h
-        while l2 and c:
-            c.val += l2.val
-            l2 = l2.next
-            if not c.next: break
-            c = c.next
-        if l2:
-            c.next = l2
+        h = l1 ## head node 고정
         
+        ## l1에 l2를 더하기
+        ## l2가 더 길면 l1뒤에 이어붙이기
+        while l2 and l1:
+            l1.val += l2.val
+            l2 = l2.next
+            if not l1.next: break
+            l1 = l1.next
+        if l2:
+            l1.next = l2
+        
+        ## 각 노드 값이 10 이상이면,
+        ## 현재 노드의 next에 1 더하기 (next가 없으면 추가)
         c = h
         while c:
             if c.val>=10:
