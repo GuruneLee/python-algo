1929fcec8e88df7cda60eb0771b9e14f1cf25b4c
ChangHa Lee <dlckdgk4858@gmail.com>
2022-09-29 22:18:13 +0900

    update

---
+++ leet/daily/220929.658.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        return sorted(sorted(arr, key=lambda a: abs(a-x))[:k])
